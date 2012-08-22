3bb8ff07836d21a16cc871e31ac36f6306356930
Farooq Mela <farooq.mela@gmail.com>
2012-08-21 18:39:45 -0700

    Reformat.

--- include/dict.h
+++ include/dict.h
@@ -37,9 +37,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define DICT_VERSION_MAJOR        0
-#define DICT_VERSION_MINOR        3
-#define DICT_VERSION_PATCH        0
+#define DICT_VERSION_MAJOR	0
+#define DICT_VERSION_MINOR	3
+#define DICT_VERSION_PATCH	0
 
 #if defined(__cplusplus) || defined(c_plusplus)
 # define BEGIN_DECL     extern "C" {
@@ -55,21 +55,21 @@ BEGIN_DECL
  * negative value if k1<k2, a positive value if k1>k2, and zero if the keys are
  * equal. The comparison should be reflexive (k1>k2 implies k1<k2, etc.),
  * symmetric (k1=k1) and transitive (k1>k2 and k2>k3 -> k1>k3). */
-typedef int         (*dict_compare_func)(const void *, const void *);
+typedef int	    (*dict_compare_func)(const void *, const void *);
 /* A pointer to a function that is called when a key-value pair gets removed
  * from a dictionary. */
-typedef void        (*dict_delete_func)    (void *, void *);
+typedef void	    (*dict_delete_func)(void *, void *);
 /* A pointer to a function used for iterating over dictionary contents. */
-typedef int         (*dict_visit_func)    (const void *, void *);
+typedef bool	    (*dict_visit_func)(const void *, void *);
 /* A pointer to a function that returns the hash value of a key. */
-typedef unsigned    (*dict_hash_func)    (const void *);
+typedef unsigned    (*dict_hash_func)(const void *);
 /* A pointer to a function that returns the priority of a key/value pair. */
-typedef unsigned    (*dict_prio_func)    (const void *, const void *);
+typedef unsigned    (*dict_prio_func)(const void *, const void *);
 
 /* A pointer to a function that libdict will use to allocate memory. */
-void*               (*dict_malloc_func)(size_t);
+void*		    (*dict_malloc_func)(size_t);
 /* A pointer to a function that libdict will use to deallocate memory. */
-void                (*dict_free_func)(void *);
+void		    (*dict_free_func)(void *);
 
 /* Forward declarations for transparent types dict and dict_itor. */
 typedef struct dict dict;
@@ -77,11 +77,11 @@ typedef struct dict_itor dict_itor;
 
 typedef dict_itor*  (*dict_inew_func)(void *obj);
 typedef size_t      (*dict_dfree_func)(void *obj);
-typedef int         (*dict_insert_func)(void *obj, void *key, void *datum,
-                                        bool overwrite);
-typedef int         (*dict_probe_func)(void *obj, void *key, void **datum);
+typedef int	    (*dict_insert_func)(void *obj, void *key, void *datum,
+					bool overwrite);
+typedef int	    (*dict_probe_func)(void *obj, void *key, void **datum);
 typedef void*       (*dict_search_func)(void *obj, const void *key);
-typedef bool        (*dict_remove_func)(void *obj, const void *key);
+typedef bool	    (*dict_remove_func)(void *obj, const void *key);
 typedef size_t      (*dict_clear_func)(void *obj);
 typedef size_t      (*dict_traverse_func)(void *obj, dict_visit_func visit);
 typedef size_t      (*dict_count_func)(const void *obj);
@@ -98,57 +98,59 @@ typedef struct {
     dict_count_func     count;
 } dict_vtable;
 
-typedef void        (*dict_ifree_func)(void *itor);
-typedef int         (*dict_valid_func)(const void *itor);
-typedef void        (*dict_invalidate_func)(void *itor);
-typedef int         (*dict_next_func)(void *itor);
-typedef int         (*dict_prev_func)(void *itor);
-typedef int         (*dict_nextn_func)(void *itor, size_t count);
-typedef int         (*dict_prevn_func)(void *itor, size_t count);
-typedef int         (*dict_first_func)(void *itor);
-typedef int         (*dict_last_func)(void *itor);
-typedef void*       (*dict_key_func)(void *itor);
-typedef void*       (*dict_data_func)(void *itor);
-typedef int         (*dict_dataset_func)(void *itor, void *datum,
-                                         void **old_datum);
-typedef int         (*dict_iremove_func)(void *itor);
-typedef int         (*dict_icompare_func)(void *itor1, void *itor2);
+typedef void	    (*dict_ifree_func)(void *itor);
+typedef int	    (*dict_valid_func)(const void *itor);
+typedef void	    (*dict_invalidate_func)(void *itor);
+typedef int	    (*dict_next_func)(void *itor);
+typedef int	    (*dict_prev_func)(void *itor);
+typedef int	    (*dict_nextn_func)(void *itor, size_t count);
+typedef int	    (*dict_prevn_func)(void *itor, size_t count);
+typedef int	    (*dict_first_func)(void *itor);
+typedef int	    (*dict_last_func)(void *itor);
+typedef void*	    (*dict_key_func)(void *itor);
+typedef void*	    (*dict_data_func)(void *itor);
+typedef int	    (*dict_dataset_func)(void *itor, void *datum,
+					 void **old_datum);
+typedef int	    (*dict_iremove_func)(void *itor);
+typedef int	    (*dict_icompare_func)(void *itor1, void *itor2);
 
 typedef struct {
-    dict_ifree_func         ifree;
-    dict_valid_func         valid;
+    dict_ifree_func	    ifree;
+    dict_valid_func	    valid;
     dict_invalidate_func    invalid;
-    dict_next_func          next;
-    dict_prev_func          prev;
-    dict_nextn_func         nextn;
-    dict_prevn_func         prevn;
-    dict_first_func         first;
-    dict_last_func          last;
-    dict_key_func           key;
-    dict_data_func          data;
+    dict_next_func	    next;
+    dict_prev_func	    prev;
+    dict_nextn_func	    nextn;
+    dict_prevn_func	    prevn;
+    dict_first_func	    first;
+    dict_last_func	    last;
+    dict_key_func	    key;
+    dict_data_func	    data;
     dict_dataset_func       dataset;
     dict_iremove_func       remove;
     dict_icompare_func      compare;
 } itor_vtable;
 
 struct dict {
-    void*           _object;
+    void*	    _object;
     dict_vtable*    _vtable;
 };
 
 #define dict_private(dct)       ((dct)->_object)
-#define dict_insert(dct,k,d,o)  ((dct)->_vtable->insert((dct)->_object, (k), (d), (o)))
-#define dict_probe(dct,k,d)     ((dct)->_vtable->probe((dct)->_object, (k), (d)))
+#define dict_insert(dct,k,d,o)  \
+    ((dct)->_vtable->insert((dct)->_object, (k), (d), (o)))
+#define dict_probe(dct,k,d)     \
+    ((dct)->_vtable->probe((dct)->_object, (k), (d)))
 #define dict_search(dct,k)      ((dct)->_vtable->search((dct)->_object, (k)))
 #define dict_remove(dct,k)      ((dct)->_vtable->remove((dct)->_object, (k)))
 #define dict_traverse(dct,f)    ((dct)->_vtable->traverse((dct)->_object, (f)))
-#define dict_count(dct)         ((dct)->_vtable->count((dct)->_object))
-#define dict_clear(dct)         ((dct)->_vtable->clear((dct)->_object))
+#define dict_count(dct)		((dct)->_vtable->count((dct)->_object))
+#define dict_clear(dct)		((dct)->_vtable->clear((dct)->_object))
 void dict_free(dict *dct);
 #define dict_itor_new(dct)      (dct)->_vtable->inew((dct)->_object)
 
 struct dict_itor {
-    void*           _itor;
+    void*	    _itor;
     itor_vtable*    _vtable;
 };
 
@@ -162,19 +164,19 @@ struct dict_itor {
 #define dict_itor_first(i)      ((i)->_vtable->first((i)->_itor))
 #define dict_itor_last(i)       ((i)->_vtable->last((i)->_itor))
 #define dict_itor_search(i,k)   ((i)->_vtable->search((i)->_itor, (k)))
-#define dict_itor_key(i)        ((i)->_vtable->key((i)->_itor))
+#define dict_itor_key(i)	((i)->_vtable->key((i)->_itor))
 #define dict_itor_data(i)       ((i)->_vtable->data((i)->_itor))
 #define dict_itor_set_data(i,d,old) \
-                                ((i)->_vtable->setdata((i)->_itor, (d), (old)))
-#define dict_itor_remove(i)            ((i)->_vtable->remove((i)->_itor))
+				((i)->_vtable->setdata((i)->_itor, (d), (old)))
+#define dict_itor_remove(i)	((i)->_vtable->remove((i)->_itor))
 void dict_itor_free(dict_itor *itor);
 
-int        dict_int_cmp(const void *k1, const void *k2);
-int        dict_uint_cmp(const void *k1, const void *k2);
-int        dict_long_cmp(const void *k1, const void *k2);
-int        dict_ulong_cmp(const void *k1, const void *k2);
-int        dict_ptr_cmp(const void *k1, const void *k2);
-int        dict_str_cmp(const void *k1, const void *k2);
+int dict_int_cmp(const void *k1, const void *k2);
+int dict_uint_cmp(const void *k1, const void *k2);
+int dict_long_cmp(const void *k1, const void *k2);
+int dict_ulong_cmp(const void *k1, const void *k2);
+int dict_ptr_cmp(const void *k1, const void *k2);
+int dict_str_cmp(const void *k1, const void *k2);
 
 END_DECL
 
--- src/dict_private.h
+++ src/dict_private.h
@@ -14,33 +14,33 @@
 # include <stdio.h>
 # undef ASSERT
 # if defined(__GNUC__)
-#  define ASSERT(expr)                                                      \
-    if (!(expr))                                                            \
-        fprintf(stderr, "\n%s:%d (%s) assertion failed: `%s'\n",            \
-                __FILE__, __LINE__, __PRETTY_FUNCTION__, #expr),            \
-        abort()
+#  define ASSERT(expr) \
+    if (!(expr)) \
+	fprintf(stderr, "\n%s:%d (%s) assertion failed: `%s'\n", \
+		__FILE__, __LINE__, __PRETTY_FUNCTION__, #expr), \
+	abort()
 # else
-#  define ASSERT(expr)                                                      \
-    if (!(expr))                                                            \
-        fprintf(stderr, "\n%s:%d assertion failed: `%s'\n",                 \
-                __FILE__, __LINE__, #expr),                                 \
-        abort()
+#  define ASSERT(expr) \
+    if (!(expr)) \
+	fprintf(stderr, "\n%s:%d assertion failed: `%s'\n", \
+		__FILE__, __LINE__, #expr), \
+	abort()
 # endif
 #else
 # define ASSERT(expr)
 #endif
 
-#define MALLOC(n)    (*dict_malloc_func)(n)
-#define FREE(p)      (*dict_free_func)(p)
+#define MALLOC(n)	(*dict_malloc_func)(n)
+#define FREE(p)		(*dict_free_func)(p)
 
-#define ABS(a)          ((a) < 0 ? -(a) : (a))
-#define MIN(a,b)        ((a) < (b) ? (a) : (b))
-#define MAX(a,b)        ((a) > (b) ? (a) : (b))
-#define SWAP(a,b,v)     do { v = (a); (a) = (b); (b) = v; } while (0)
+#define ABS(a)		((a) < 0 ? -(a) : (a))
+#define MIN(a,b)	((a) < (b) ? (a) : (b))
+#define MAX(a,b)	((a) > (b) ? (a) : (b))
+#define SWAP(a,b,v)	do { v = (a); (a) = (b); (b) = v; } while (0)
 
 #if defined(__GNUC__)
-# define GCC_INLINE        __inline__
-# define GCC_CONST        __attribute__((__const__))
+# define GCC_INLINE	__inline__
+# define GCC_CONST	__attribute__((__const__))
 #else
 # define GCC_INLINE
 # define GCC_CONST
