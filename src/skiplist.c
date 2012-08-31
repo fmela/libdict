660a28d24df8c6a091b52686efb28c3b647a1bdc
Farooq Mela <farooq.mela@gmail.com>
2012-08-31 16:04:05 -0700

    Reorganize #includes.

--- src/skiplist.c
+++ src/skiplist.c
@@ -31,10 +31,9 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdlib.h>
-#include <string.h>
-
 #include "skiplist.h"
+
+#include <string.h>	    /* For memset() */
 #include "dict_private.h"
 
 typedef struct skip_node skip_node;
