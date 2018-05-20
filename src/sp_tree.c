95f6d27f277932272578b2554104e1e4414cfaac
Farooq Mela <fmela@users.noreply.github.com>
2018-05-20 13:30:56 -0700

    Improve splay tree deletion (#31)

--- src/sp_tree.c
+++ src/sp_tree.c
@@ -359,23 +359,15 @@ remove_node(sp_tree* tree, sp_node* node)
 	SWAP(node->datum, out->datum, tmp);
     }
 
-    sp_node* temp = out->llink ? out->llink : out->rlink;
-    sp_node* parent = out->parent;
+    sp_node* const temp = out->llink ? out->llink : out->rlink;
+    sp_node* const parent = out->parent;
+    FREE(out);
     if (temp)
 	temp->parent = parent;
+
     *(parent ? (parent->llink == out ? &parent->llink : &parent->rlink) : &tree->root) = temp;
-
-    /* Splay an adjacent node to the root, if possible. */
-    temp =
-	node->parent ? node->parent :
-	node->rlink ? node->rlink :
-	node->llink;
-    if (temp) {
-	splay(tree, temp);
-	ASSERT(tree->root == temp);
-    }
-
-    FREE(out);
+    if (parent)
+	splay(tree, parent);
     tree->count--;
 }
 
